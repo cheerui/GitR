8215447f9e5c79b2c2fc5e4bdeeef9221011594e
cheerui <1574225054@qq.com>
2021-01-20 14:25:23 +0800

    添加了Person类，增加了名字属性

---
+++ Test01/src/cn/cheerui/Test/Person.java
@@ -0,0 +1,11 @@
+package cn.cheerui.Test;
+
+/**
+ * @Author: Cheerui
+ * @Date: 2021/1/20
+ * @Description: cn.cheerui.Test
+ * @Version: 1.0
+ */
+public class Person {
+    private  String name;
+}
